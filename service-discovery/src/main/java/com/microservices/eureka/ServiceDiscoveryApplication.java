8cd394c82083b94492d95be1810807913823c329
Skaaan <skanderayari96@gmail.com>
2024-06-13 20:07:37 +0200

    Configure Eureka service discovery; Refactor pom.xml

--- service-discovery/src/main/java/com/microservices/eureka/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/microservices/eureka/ServiceDiscoveryApplication.java
@@ -1,9 +1,14 @@
 package com.microservices.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// https://dev.to/fullstackjava/how-to-use-spring-boot-eureka-server-in-spring-boot-330-5e5j
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
