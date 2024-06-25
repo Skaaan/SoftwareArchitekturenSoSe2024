325bd1c76240c43fe249fea4386aeedb1001c639
Skaaan <skanderayari96@gmail.com>
2024-06-25 20:15:21 +0200

    Impl OrderController

---
+++ order-service/src/main/java/com/microservices/order/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.microservices.order.controller;
+
+
+import com.microservices.order.dto.OrderRequest;
+import com.microservices.order.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Succesfully";
+    }
+}
