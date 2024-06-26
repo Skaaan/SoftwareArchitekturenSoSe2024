2f9ab3085386955eeb473bee51e5e59db7984695
Skaaan <skanderayari96@gmail.com>
2024-06-26 21:05:27 +0200

    Add InventoryRepository, InventoryService and InventoryController classes

---
+++ inventory-service/src/main/java/com/microservices/inventory/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.microservices.inventory.controller;
+
+import com.microservices.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+       return inventoryService.isInStock(skuCode);
+    }
+
+}
