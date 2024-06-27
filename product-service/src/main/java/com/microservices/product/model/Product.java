a704966f877b4fe3c5dbead36e2e441fafb8e07c
Skaaan <skanderayari96@gmail.com>
2024-06-27 18:26:25 +0200

    Add Product model class

---
+++ product-service/src/main/java/com/microservices/product/model/Product.java
@@ -0,0 +1,28 @@
+package com.microservices.product.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageLink;
+}
