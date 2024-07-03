335e2f6dd26dcf4f69c63ec4b8f96559ccb61bac
yasminehaidri <yasmine.haidri@student.htw-berlin.de>
2024-07-03 13:13:49 +0200

    fixed configs of service/ added config for root/ added notificationService

---
+++ notification_service/src/main/java/NotificationServiceApplication.java
@@ -0,0 +1,43 @@
+package SoftwareArchitekturenSoSe2024.notification_service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationServiceApplication {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @RabbitListener(queues = "orderQueue")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Received Notification for Order - {}", orderPlacedEvent.getOrderNumber());
+        sendEmail(orderPlacedEvent.getUserEmail(), "Order Confirmation", 
+                  "Thank you for your order. Your order number is " + orderPlacedEvent.getOrderNumber());
+    }
+    
+    /* 
+    private void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom(fromEmail);
+        mailSender.send(message);
+    }
+    */
+}
