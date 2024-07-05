bf729c2b30d61a7eaa488e2aeff372d00532f1bb
Skaaan <skanderayari96@gmail.com>
2024-07-05 18:26:02 +0200

    Update RabbitMQConfig

--- product-service/src/main/java/com/microservices/product/config/RabbitMQConfig.java
+++ product-service/src/main/java/com/microservices/product/config/RabbitMQConfig.java
@@ -4,28 +4,43 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${rabbitmq.exchange.name}")
-    private String exchangeName;
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(exchangeName);
-    }
+    public static final String EXCHANGE_NAME = "stock.check.exchange";
+    public static final String PRODUCT_QUEUE_NAME = "stock.check.product.queue";
+    public static final String PRODUCT_ROUTING_KEY = "product.created";
 
     @Bean
     public Queue productQueue() {
-        return new Queue("product.queue");
+        return new Queue(PRODUCT_QUEUE_NAME);
     }
 
     @Bean
-    public Binding binding(Queue productQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(productQueue).to(exchange).with("product.routing.key");
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding bindingProductQueue(Queue productQueue, TopicExchange exchange) {
+        return BindingBuilder.bind(productQueue).to(exchange).with(PRODUCT_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 }
