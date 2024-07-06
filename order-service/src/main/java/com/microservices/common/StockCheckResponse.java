df7597c23b8d4b573ea3d5e36d9034773eb135cc
Skaaan <skanderayari96@gmail.com>
2024-07-06 18:51:45 +0200

    Add StockCheckResponse

---
+++ order-service/src/main/java/com/microservices/common/StockCheckResponse.java
@@ -0,0 +1,13 @@
+package com.microservices.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockCheckResponse {
+    private String skuCode;
+    private boolean inStock;
+}
