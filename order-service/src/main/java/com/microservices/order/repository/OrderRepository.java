46d0aeb47338d3e176a60633bf06882e88028301
Skaaan <skanderayari96@gmail.com>
2024-07-17 13:20:34 +0200

    Refactor OrderRepository

--- order-service/src/main/java/com/microservices/order/repository/OrderRepository.java
+++ order-service/src/main/java/com/microservices/order/repository/OrderRepository.java
@@ -2,6 +2,12 @@ package com.microservices.order.repository;
 
 import com.microservices.order.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o JOIN FETCH o.orderLineItemsList WHERE o.id = :id")
+    Optional<Order> findByIdWithItems(@Param("id") Long id);
 }
